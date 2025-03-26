2d1a43aabe727e932569763e8725197e61dc4b13
abanoub-refaat <abanoubref3at18@gmail.com>
2025-03-26 22:19:32 +0200

    upadate _layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Redirect,Stack } from "expo-router";
-
-
+import { Redirect, Stack } from "expo-router";
 
 const _layout = () => {
   return (
     <>
-    <Redirect href="/screens/firstpage" />
-    <Stack>
-      <Stack.Screen name="firstpage" options={{ title: "firstpage", headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
-  </>
+      {/* <Redirect href="/screens/firstpage" /> */}
+      <Stack>
+        <Stack.Screen
+          name="firstpage"
+          options={{ title: "firstpage", headerShown: false }}
+        />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </>
   );
 };
 
 export default _layout;
-
